0ac9485498d9d278b2f0164656652a1026644495
MESSAHEL Zinédine <zinedine.messahel.ext@celine.fr>
2023-01-12 11:40:25 +0100

    feat: routing ok

--- src/index.js
+++ src/index.js
@@ -2,43 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-//import Home from './views/Home';
+import Home from './views/Home';
 import Community from './views/Community';
 import Profile from './views/Profile';
 import NoMatch from './views/NoMatch';
-import App from "./App";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Navigate to="/home" replace />,
+    element: <Home />
+  },
+  {
+    path: '/profile',
+    element: <Profile />
+  },
+  {
+    path: '/community',
+    element: <Community />,
     children: [
       {
-        path: '/home',
-        element: <App />
-      },
-      {
-        path: '/community',
-        element: <Community />,
-        children: [
-          {
-            path: '/community/profile',
-            element: <Profile role="user" />,
-            role: 'user'
-          }
-        ]
-      },
-      {
-        path: '/profile',
-        element: <Profile />
-      },
-      { path: '*', element: <NoMatch /> }
+        path: '/community/profile',
+        element: <Profile role="user" />,
+        role: 'user'
+      }
     ]
-  }
+  },
+  { path: '*', element: <NoMatch /> }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
