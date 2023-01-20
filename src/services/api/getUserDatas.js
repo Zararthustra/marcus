afb8ba43b8630a2d2259c2513cf1f366bf7456ae
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2023-01-20 15:14:08 +0100

    setup generic axios requests

--- src/services/api/getUserDatas.js
+++ src/services/api/getUserDatas.js
@@ -1,17 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+
+const PROTOCOL = { unsecure: "http", secure: "https" };
+const HOST = { ip: "13.39.16.245", domain: "marcusback.arthurmayer.fr" };
+
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+axios.defaults.baseURL = PROTOCOL.unsecure + "://" + HOST.ip + "/api";
 
 export const getMasterpieces = (userId) => {
-    return axios.get(`https://marcusback.arthurmayer.fr/api/masterpieces?user_id=${userId}`)
-}
+  return axios.get(`/masterpieces?user_id=${userId}`);
+};
 
 export const getWatchlists = (userId) => {
-    return axios.get(`https://marcusback.arthurmayer.fr/api/watchlists?user_id=${userId}`)
-}
+  return axios.get(`/watchlists?user_id=${userId}`);
+};
 
 export const getVotes = (userId) => {
-    return axios.get(`https://marcusback.arthurmayer.fr/api/votes?user_id=${userId}`)
-}
+  return axios.get(`/votes?user_id=${userId}`);
+};
 
 export const getCritics = (userId) => {
-    return axios.get(`https://marcusback.arthurmayer.fr/api/critics?user_id=${userId}`)
-}
+  return axios.get(`/critics?user_id=${userId}`);
+};
