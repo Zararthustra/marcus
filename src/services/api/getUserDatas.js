2052954f3ea5b10ef2679c389f67f981f6d889cb
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2023-01-20 15:17:45 +0100

    add future bearer token header

--- src/services/api/getUserDatas.js
+++ src/services/api/getUserDatas.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const PROTOCOL = { unsecure: "http", secure: "https" };
 const HOST = { ip: "13.39.16.245", domain: "marcusback.arthurmayer.fr" };
+// const token = getLocalStorage('accessToken')
 
+//axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-axios.defaults.baseURL = PROTOCOL.unsecure + "://" + HOST.ip + "/api";
+axios.defaults.baseURL = PROTOCOL.secure + "://" + HOST.ip + "/api";
 
 export const getMasterpieces = (userId) => {
   return axios.get(`/masterpieces?user_id=${userId}`);
