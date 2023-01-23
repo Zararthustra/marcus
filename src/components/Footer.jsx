f21df5392836fb2e053a7f3af8c5f4ccd80441ce
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2023-01-24 00:16:51 +0100

    fix CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,12 @@ import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="app-footer">
       <p>
         This product uses the TMDB API but is not endorsed or certified by
         <a href="https://www.themoviedb.org/"> TMDB. </a>
       </p>
+      <p className="ARR">All Rights Reserved © Marcus 2023</p>
     </footer>
   );
 };
