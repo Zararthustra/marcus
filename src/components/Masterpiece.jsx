c54539872b86895ce247c683c83921b5043cbfd2
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2023-01-26 10:06:46 +0100

    refactor(styles): minor fixes on masterpiece

--- src/components/Masterpiece.jsx
+++ src/components/Masterpiece.jsx
@@ -4,50 +4,51 @@ import { ReactComponent as CriticWatchList } from "../assets/svg/criticWatchList
 import "../styles/Masterpiece.css";
 
 const Masterpiece = ({
-    movieName,
-    movieId,
-    releasedDate,
-    director,
-    description,
-    userName,
-    userId,
-    poster,
-    }) => {
-    const [triggerToast, setTriggerToast] = useState(false);
+  movieName,
+  movieId,
+  releasedDate,
+  director,
+  description,
+  userName,
+  userId,
+  poster,
+}) => {
+  const [triggerToast, setTriggerToast] = useState(false);
+  const [showMovie, setShowMovie] = useState(false);
 
-    return (
-        <article className="masterpiece">
-        <div className="masterpiece-left">
-            <img src={poster} alt={movieName} />
-        </div>
-        <div className="masterpiece-right">
-            <header>
-            <a href={`/movie/${movieId}`}>
-                <h3 className="masterpiece-title">{movieName}</h3>
+  return (
+    <article className="masterpiece">
+      <div className="masterpiece-left">
+        <img src={poster} alt={movieName} />
+      </div>
+      <div className="masterpiece-right">
+        <header>
+          <h2 onClick={() => setShowMovie(true)} className="masterpiece-title">
+            {movieName}
+          </h2>
+        </header>
+        <main>
+          <div className="masterpiece-release-director">
+            <p>{releasedDate}</p>
+            <p>{director}</p>
+          </div>
+          <p className="masterpiece-description">{description}</p>
+        </main>
+        <footer className="masterpiece-footer">
+          <p className="masterpiece-footer-userName">
+            <a href={`/users/${userId}`}>{userName}</a>
+          </p>
+          <div className="masterpiece-footer-icons">
+            <CriticWatchList onClick={() => setTriggerToast(!triggerToast)} />
+            {triggerToast && <p message={`Added ${movieName} to watchlist`} />}
+            <a href={`/movies/${movieId}`}>
+              <CriticInfo />
             </a>
-            </header>
-            <main>
-            <div className="masterpiece-release-director">
-                <p>{releasedDate}</p>
-                <p>{director}</p>
-            </div>
-            <p className="masterpiece-description">{description}</p>
-            </main>
-            <footer className="masterpiece-footer">
-            <p className="masterpiece-footer-userName">
-                <a href={`/users/${userId}`}>{userName}</a>
-            </p>
-            <div className="masterpiece-footer-icons">
-                <CriticWatchList onClick={() => setTriggerToast(!triggerToast)} />
-                {triggerToast && <p message={`Added ${movieName} to watchlist`} />}
-                <a href={`/movies/${movieId}`}>
-                <CriticInfo />
-                </a>
-            </div>
-            </footer>
-        </div>
-        </article>
-    );
+          </div>
+        </footer>
+      </div>
+    </article>
+  );
 };
 
 export default Masterpiece;
